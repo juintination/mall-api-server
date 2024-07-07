cc4a4c719d0a4b8dc6dfaa0af099552b2ad9f7e7
DEOKJAE KWON <juintin@kakao.com>
2024-07-07 16:58:53 +0900

    feat: add a framework of the project

---
+++ src/main/java/org/zerock/mallapi/MallApiApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallApiApplication.class, args);
+	}
+
+}
